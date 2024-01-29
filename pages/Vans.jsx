5471af49afb8d8dd4352d2aff1e453886643a7c7
Maria Arkhipova <marias1403@yandex.ru>
2024-01-29 20:52:17 +0900

    feat: vans page

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,22 +1,32 @@
-import React from "react"
-
-/**
- * Challenge: Fetch and map over the data to display it on
- * the vans page. For an extra challenge, spend time styling
- * it to look like the Figma design.
- * 
- * Hints:
- * 1. Use `fetch(/api/vans)` to kick off the request to get the
- *    data from our fake Mirage JS server
- * 2. What React hook would you use to fetch data as soon as the
- *    Vans page loads, and only fetch it the one time?
- * 3. You may get an error saying "console.groupCollapsed is not
- *    a function". You can ignore it for now.
- */
-
+import React, { useEffect, useState } from 'react';
 
 export default function Vans() {
-    return (
-        <h1>Vans page goes here 🚐</h1>
-    )
+  const [vans, setVans] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/vans')
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  const vanElements = vans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} alt={van.name} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ));
+
+  return (
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <div className="van-list">{vanElements}</div>
+    </div>
+  );
 }
