713cf628e3862704e63a5fb7cc5ff5bbaf677183
Maria Arkhipova <marias1403@yandex.ru>
2024-02-15 21:37:21 +0900

    refactor: Link to NavLink

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
@@ -8,9 +8,24 @@ export default function Header() {
         #VanLife
       </Link>
       <nav>
-        <Link to="/host">Host</Link>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
+        <NavLink
+          to="/host"
+          className={({ isActive }) => (isActive ? 'active-link' : '')}
+        >
+          Host
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? 'active-link' : '')}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/vans"
+          className={({ isActive }) => (isActive ? 'active-link' : '')}
+        >
+          Vans
+        </NavLink>
       </nav>
     </header>
   );
