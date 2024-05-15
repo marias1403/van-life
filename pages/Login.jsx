cea34db9d150dec44cb7a35e43478c56aaa32bb6
Maria Arkhipova <marias1403@yandex.ru>
2024-05-15 20:00:48 +0900

    feat: action error handling

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { Form, useLoaderData, useNavigate } from 'react-router-dom';
+import {
+  Form,
+  redirect,
+  useActionData,
+  useLoaderData,
+  useNavigate,
+} from 'react-router-dom';
 import { loginUser } from '../api';
 
 export function loader({ request }) {
@@ -10,25 +16,26 @@ export async function action({ request }) {
   const formData = await request.formData();
   const email = formData.get('email');
   const password = formData.get('password');
-  const data = await loginUser({ email, password });
-  localStorage.setItem('loggedIn', true);
-
-  return null;
+  try {
+    const data = await loginUser({ email, password });
+    localStorage.setItem('loggedIn', true);
+    return redirect('/host');
+  } catch (err) {
+    return err.message;
+  }
 }
 
 export default function Login() {
   const [status, setStatus] = useState('idle');
-  const [error, setError] = useState(null);
+  const errorMessage = useActionData();
   const message = useLoaderData();
   const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     setStatus('submitting');
-    setError(null);
     loginUser(loginFormData)
       .then(() => navigate('/host', { replace: true }))
-      .catch((err) => setError(err))
       .finally(() => setStatus('idle'));
   }
 
@@ -36,8 +43,8 @@ export default function Login() {
     <div className="login-container">
       <h1>Sign in to your account</h1>
       {message && <h3 className="red">{message}</h3>}
-      {error && <h3 className="red">{error.message}</h3>}
-      <Form method="post" className="login-form">
+      {errorMessage && <h3 className="red">{errorMessage}</h3>}
+      <Form method="post" className="login-form" replace>
         <input name="email" type="email" placeholder="Email address" />
         <input name="password" type="password" placeholder="Password" />
         <button disabled={status === 'submitting'}>
